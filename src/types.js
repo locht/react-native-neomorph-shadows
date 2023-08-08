ebba5db1596e23844d7d842c93540b8fea2d5189
locht <thanhloclhu@gmail.com>
2023-08-08 09:00:52 +0700

    fix the error about Invariant Violation: ViewPropTypes has been removed from React Native

--- src/types.js
+++ src/types.js
@@ -1,49 +1,49 @@
-import { ViewPropTypes } from 'react-native';
+import { ViewPropTypes } from 'deprecated-react-native-prop-types';
 import { number, shape, string, node, bool } from 'prop-types';
 
 export const ShadowARTType = {
-  width: number.isRequired,
-  height: number.isRequired,
-  borderRadius: number,
-  shadowRadius: number,
-  shadowOffset: shape({
-    x: number,
-    y: number,
-  }),
-  shadowOpacity: number,
-  shadowColor: string,
-  backgroundColor: string,
+    width: number.isRequired,
+    height: number.isRequired,
+    borderRadius: number,
+    shadowRadius: number,
+    shadowOffset: shape({
+        x: number,
+        y: number,
+    }),
+    shadowOpacity: number,
+    shadowColor: string,
+    backgroundColor: string,
 };
 
 export const InnerShadowType = {
-  ...ViewPropTypes,
-  children: node,
-  style: shape({
-    ...ViewPropTypes.style,
-    width: number.isRequired,
-    height: number.isRequired,
-  }),
+    ...ViewPropTypes,
+    children: node,
+    style: shape({
+        ...ViewPropTypes.style,
+        width: number.isRequired,
+        height: number.isRequired,
+    }),
 };
 
 export const OuterShadowType = {
-  ...InnerShadowType,
-  inner: bool,
-  useArt: bool,
+    ...InnerShadowType,
+    inner: bool,
+    useArt: bool,
 };
 
 export const NeomorphType = {
-  ...OuterShadowType,
-  darkShadowColor: string,
-  lightShadowColor: string,
-  swapShadows: bool,
+    ...OuterShadowType,
+    darkShadowColor: string,
+    lightShadowColor: string,
+    swapShadows: bool,
 };
 
 export const NeomorphFlexType = {
-  ...NeomorphType,
-  style: ViewPropTypes.style,
+    ...NeomorphType,
+    style: ViewPropTypes.style,
 };
 
 export const ShadowFlexType = {
-  ...OuterShadowType,
-  style: ViewPropTypes.style,
+    ...OuterShadowType,
+    style: ViewPropTypes.style,
 };
